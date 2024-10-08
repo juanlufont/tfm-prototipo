74bad4633e0091538a167febbcf42448726b45e4
Juan Luis Font <juanluis.fontcalvo@surf.nl>
2024-10-08 14:17:07 +0200

    Update template

--- filelist.sh
+++ filelist.sh
@@ -25,22 +25,27 @@ draft: false
 Esta página contiene un listado de enlaces almacenados en la instancia
 local IPFS incluida en el prototipo.
 
-Los enlaces apuntan a copias de las imágenes usadas en el presente
-TFM (las cuales se incluyen en el documento PDF final).
+Los enlaces apuntan a colección de libros bajo Dominio Público y artículos
+publicados como Open Access.Estos archivos están alojados por una instancia
+temporal (self-hosted) de IPFS. El acceso a los archivos se puede realizar a
+través de dos enlaces distintos, uno al _gateway_ local de la instancia IPFS y
+otro al _gateway_ HTTPS proporcionada por el proyecto IPFS.
 
-Estas imágenes están alojadas por una instancia temporal "auto-alojada"
-(self-hosted) de IPFS. El acceso a los archivos se puede realizar a través de
-dos enlaces distintos, uno al _gateway_ local de la instancia IPFS y otro al
-_gateway_ HTTPS proporcionada por el proyecto IPFS.
-
-**AVISO!** debido a restricciones del _gateway_ público, los archivos PDF
-presentan más problemas a la hora de ser accecidos a través de estos
-enlaces. Este problema se puede solventar con configuraciones avanzadas de
-red, fuera del objetivo de este prototipo.
+---
 
 EOF
 
 # write list of IPFS urls
 while read -r file cid _; do
-    echo "- [${file}](${DOMAIN}/ipfs/${cid}?filename=${file}), [mirror](https://ipfs.io/ipfs/${cid}?filename=${file})"
+  echo "- [${file}](${DOMAIN}/ipfs/${cid}?filename=${file}), [mirror](https://ipfs.io/ipfs/${cid}?filename=${file})"
 done <<<"${DATA}"
+
+cat <<EOF
+
+---
+
+**AVISO!** debido a restricciones del _gateway_ público, los archivos PDF y
+ePUB presentan más problemas a la hora de ser accecidos a través de estos
+enlaces. Este problema puede solventarse con configuraciones avanzadas de red,
+fuera del objetivo de este prototipo.
+EOF
